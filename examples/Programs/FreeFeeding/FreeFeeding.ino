9679abb3d92f610ecb837ee1dc6345ef7a24c542
Kravitz Lab <alexxai@wustl.edu>
2024-12-26 09:50:41 -0500

    Added example programs

--- examples/Programs/FreeFeeding/FreeFeeding.ino
+++ examples/Programs/FreeFeeding/FreeFeeding.ino
@@ -1,11 +1,10 @@
 /*
   Feeding experimentation device 4 (FED4)
 
-  This "Fixed-Ratio 1" or "FR1" program will dispense a pellet 
-  each time the mouse touches the Left poke. 
+  This "FreeFeeding" program will dispense a pellet automatically
+  and replace it each time that it is removed. 
   
-  This is useful for quantifying simple learning rates and 
-  accuracy.
+  This is useful for quantifying total pellet intake.
 */
 
 #include <FED4.h>               // include the FED4 library
@@ -13,7 +12,6 @@
 FED4 fed4;                      // start FED4 object
 
 // TODO: needs a method for updating the JSON "program" item
-// TODO: add conditioned stimuli and poke logging
 
 void setup() {
   fed4.begin();                 // initialize FED4 hardware
@@ -21,8 +19,5 @@ void setup() {
 
 void loop() {
   fed4.run();                   // run this once per loop
-  
-  if (fed4.leftTouch) {         // if left poke is touched
-    fed4.feed();                // feed one pellet
-  }
+  fed4.feed();                  // feed one pellet
 }
---
+++ examples/Programs/ResettingPR1/ResettingPR1.ino
@@ -0,0 +1,56 @@
+/*
+  Feeding experimentation device 4 (FED4)
+
+  This is a resetting progressive ratio task meant to be used in a closed economy 
+  setting as in Mourra et al., 2020: https://pubmed.ncbi.nlm.nih.gov/31809732/
+  
+  The task starts by requiring one poke on the Left (ie: FR1) to dispense a pellet.
+  After the first poke the requirement incrememnts by 1 for each earned pellet (ie: FR1, FR3, FR5, etc.)
+  If 30 minutes pass without a left or right poke the requirement resets to FR1 and the game starts again.
+  
+  This is useful for quantifying the "thriftiness" in their strategy for getting pellets.
+
+*/
+
+#include <FED4.h>  // include the FED4 library
+
+FED4 fed4;  // start FED4 object
+
+//store local variables for this task
+int poke_num = 0;                                        //number of pokes since last pellet
+int pokes_required = 1;                                  //current FR
+unsigned long poketime = 0;                              //time of poke
+int resetInterval = 30;                                  //number of minutes without a poke before requirement resets to FR1
+
+// TODO: needs a method for updating the JSON "program" item
+// TODO: add conditioned stimuli and poke logging
+
+void setup() {
+  fed4.begin();                                          // initialize FED4 hardware
+}
+
+void loop() {
+  fed4.run();                                            // run this once per loop
+  checkReset();                                          //Check if it's time to reset to FR1
+  if (fed4.leftTouch) {                                  // if left poke is touched
+    poke_num++;                                          //increment poke number.
+    poketime = fed4.unixtime;                            //update the current time of poke
+    if (poke_num == pokes_required) {                    //check if current FR has been achieved
+      fed4.feed();                                       //Deliver pellet
+      pokes_required += 1;                               //Edit this line to change the PR incremementing formula.  Default is for each pellet add 1 to the pokes required.
+      poke_num = 0;                                      //reset poke_num to 0
+    }
+  }
+
+  if (fed4.rightTouch) {                                 //If right poke is triggered
+  }
+}
+
+// check if resetInterval has elapsed since last poke
+void checkReset() {
+  if (fed4.unixtime - poketime >= (resetInterval*60)) {  // if the reset interval has elapsed since last poke
+    poke_num = 0;                                        // set the poke_num back to 0
+    pokes_required = 1;                                  // set the ratio back to FR1
+    poketime = fed4.unixtime;                            // update the time of the last poke
+  }
+}
