eb3f5d0e764c2c6c15e5c783a3cb0218e12f8d8b
Kravitz Lab <alexxai@wustl.edu>
2025-07-27 22:12:12 +0100

    Update FED4_Serial.cpp

--- src/FED4_Serial.cpp
+++ src/FED4_Serial.cpp
@@ -5,7 +5,7 @@ void FED4::serialStatusReport()
     if (wakeCount == 0) {
         Serial.print("Ready to go!");
     }
-    Serial.printf(" - Wake %d | %02d/%02d/%02d %02d:%02d:%02d | Temp %.1fC | Hum %.1f%% | Lux %.3f | White %.3f | Bat %.1fV(%.1f%%) | Motion %d(%.1f%%) | Pellets %d | Left/Center/Right %d/%d/%d | FreeHeap %d\n",        
+    Serial.printf(" - Wake %d | %02d/%02d/%02d %02d:%02d:%02d | %.1fC | RH %.1f%% | Lux %.3f (%.3f) | Bat %.1fV(%.1f%%) | Motion %d(%.1f%%) | Pellets %d | Left/Center/Right %d/%d/%d | FreeMem %d\n",        
         wakeCount,
         rtc.now().month(), rtc.now().day(), rtc.now().year(), rtc.now().hour(), rtc.now().minute(), rtc.now().second(),
         temperature, humidity, lux, white,
